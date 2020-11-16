e216054485e183bba2f5ee831977bcb0a7c3b9eb
benjaminGUYT <55363637+benjaminGUYT@users.noreply.github.com>
2020-11-16 17:42:16 +0100

    last fix i hope

--- src/Entity/exercices/RefPrenoms.php
+++ src/Entity/exercices/RefPrenoms.php
@@ -13,7 +13,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\RefPrenomsRepository")
  * @ApiResource
- * @ApiFilter(SearchFilter::class, properties={"label": "partial"})
+ * @ApiFilter(RefPrenoms::class, properties={"label": "partial"})
  */
 class RefPrenoms implements FilterInterface
 {
